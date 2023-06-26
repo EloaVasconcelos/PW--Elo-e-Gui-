147b21aeeb97719b10c6af22b3a5328df9801f19
EloáVM22 <127448572+EloaVasconcelos@users.noreply.github.com>
2023-06-26 08:27:56 -0300

    Add files via upload
    AULA DO DIA 26/06 PH  - NOVA DUPLA

---
+++ exeForm/BDPWA.sql
@@ -0,0 +1,13 @@
+Create database dbBDPWA;
+use dbBDPWA;
+
+create table tbcliente(
+Codigo int primary key auto_increment,
+Nome varchar(50) not null,
+Senha char(8) not null,
+Sexo char(1) not null,
+Estado char(2) not null
+);
+
+
+describe tbcliente;
